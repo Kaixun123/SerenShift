76ce7909bfb10549c682d05e5a89d0fb5883ec50
Juay Kai Xun <juaykaixun@hotmail.com>
2024-09-14 22:10:35 +0800

    chore: fix branch validator name

--- validate-branch-name.config.js
+++ validate-branch-name.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branchNameRegex: /^(feature|features|fix|hotfix|release|chore)\/[a-zA-Z0-9._-]+$/,
+    branchNameRegex: /^(master|main|develop)$|^(feature|fix|hotfix|release|chore)\/[a-zA-Z0-9._-]+$/,
     errorMsg: 'EH DIU your Branch name is invalid! It should start with feature/, bugfix/, hotfix/, release/, or chore/ followed by a descriptive name.'
 };
 
