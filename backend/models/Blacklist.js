ee9cb0fd93a47a91b363dd2c2db662c194b9e120
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-09-20 23:12:43 +0800

    feat: added new blacklist DB entity

---
+++ backend/models/Blacklist.js
@@ -0,0 +1,45 @@
+const { DataTypes, Model } = require("sequelize");
+const { sequelize } = require("../services/database/mysql");
+
+class Blacklist extends Model { }
+
+Blacklist.init(
+    {
+        blacklist_id: {
+            type: DataTypes.INTEGER(6),
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        start_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        end_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        blacklist_type: {
+            type: DataTypes.ENUM,
+            values: ['Regular', 'Ad Hoc'],
+            allowNull: false,
+        },
+        created_by: {
+            type: DataTypes.INTEGER(6),
+            allowNull: false,
+        },
+        last_update_by: {
+            type: DataTypes.INTEGER(6),
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        createdAt: 'created_timestamp',
+        updatedAt: 'last_update_timestamp',
+        tableName: "Blacklist",
+    }
+);
+
+module.exports = Blacklist;
