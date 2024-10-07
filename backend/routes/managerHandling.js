c9180d1cb99fb0dd6bdb1f8d1cd05c15f708f854
DylanChua2001 <dylanchua2001dc@gmail.com>
2024-10-07 21:47:33 +0800

    feat:SPMPROJ-15 backend for approving and rejecting application created

--- backend/routes/managerHandling.js
+++ backend/routes/managerHandling.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const managerController = require('../controllers/managerController');
+<<<<<<< HEAD
 const { ensureManager, } = require("../middlewares/authMiddleware");
 
 // Get the route to handle a GET request
 router.get("/retrievePendingApplication", ensureManager, (req, res) => managerController.retrievePendingApplication(req, res))
+=======
+const { ensureLoggedIn, ensureManager, } = require("../middlewares/authMiddleware");
+
+>>>>>>> bb83bd5 (feat:SPMPROJ-15 backend for approving and rejecting application created)
 
 // Update the route to handle a PUT request
 router.put("/approveApplication", ensureManager, (req, res) => managerController.approveApplication(req, res));
