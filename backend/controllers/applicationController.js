c529467bd94ba815e09ce506cd01925f2fe36f9b
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-08 22:09:58 +0800

    feat: initial SPMPROJ-30 initial multiple approve applications api

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -1,6 +1,6 @@
 const { Application, Employee, Schedule } = require('../models');
-const { checkforOverlap } = require('../services/common/applicationHelper');
-const { splitScheduleByDate } = require('../services/common/scheduleHelper')
+const { checkforOverlap, checkWhetherSameDate, splitDatesByDay } = require('../services/common/applicationHelper');
+const { Op } = require('sequelize');
 
 // GET function - to retrieve application data based on userId and status
 const retrieveApplication = async (req, res, next) => {
@@ -94,7 +94,88 @@ const createNewApplication = async (req, res, next) => {
     }
 }
 
+// POST function - to approve mutliple applications
+const approveApplications = async (req, res, next) => {
+    let { application_id, approvedDates, approverRemarks } = req.body;
+    try {
+        let allApplications = [];
+        let mainApplication = await Application.findByPk(application_id);
+        if (!mainApplication) {
+            return res.status(404).json({ message: "Application not found" });
+        }
+        let nextLinkedApplication = mainApplication.linked_application;
+        while (nextLinkedApplication) {
+            let linkedApplication = await Application.findByPk(nextLinkedApplication);
+            if (!linkedApplication)
+                break;
+            else if (linkedApplication.status === "Pending")
+                allApplications.push(linkedApplication);
+            nextLinkedApplication = linkedApplication.linked_application;
+        }
+        if (allApplications.length === 1)
+            return res.status(400).json({ message: "No linked applications are found with this application" });
+        let lastApprovedApplicationIndex = 0;
+        let lastCratedScheduleID = 0;
+        for (let i = 0; i < allApplications.length; i++) {
+            let currentApplication = allApplications[i];
+            let conflictingSchedule = await Schedule.findAll({
+                [Op.or]: [
+                    {
+                        start_date: {
+                            [Op.between]: [currentApplication.start_date, currentApplication.end_date]
+                        }
+                    },
+                    {
+                        end_date: {
+                            [Op.between]: [currentApplication.start_date, currentApplication.end_date]
+                        }
+                    }
+                ]
+            });
+            if (conflictingSchedule.length > 0) {
+                return res.status(400).json({ message: "Conflicting schedule found on " + currentApplication.start_date + " and ending on " + currentApplication.end_date });
+            }
+            if (checkWhetherSameDate(currentApplication.start_date, currentApplication.end_date)) {
+                if (approvedDates.some(date => checkWhetherSameDate(date, currentApplication.start_date))) {
+                    currentApplication.status = "Approved";
+                    currentApplication.verify_by = req.user.id;
+                    currentApplication.verify_timestamp = new Date();
+                    currentApplication.approver_remarks = approverRemarks;
+                    await currentApplication.save();
+                    let newSchedule = await Schedule.create({
+                        start_date: currentApplication.start_date,
+                        end_date: currentApplication.end_date,
+                        created_by: req.user.id,
+                    });
+                    lastApprovedApplicationIndex = i;
+                }
+            } else {
+                let splittedDates = splitDatesByDay(currentApplication.start_date, currentApplication.end_date);
+                let approvedDatesForCurrentApplication = approvedDates.filter(date => splittedDates.includes(date));
+                if (approvedDatesForCurrentApplication.length > 0) {
+                    currentApplication.status = "Approved";
+                    currentApplication.verify_by = req.user.id;
+                    currentApplication.verify_timestamp = new Date();
+                    currentApplication.approver_remarks = approverRemarks;
+                    await currentApplication.save();
+                    let newSchedule = await Schedule.create({
+                        start_date: currentApplication.start_date,
+                        end_date: currentApplication.end_date,
+                        created_by: req.user.id,
+                    });
+                    lastApprovedApplicationIndex = i;
+                }
+            }
+        }
+        return res.status(200).json({ message: "Applications approved successfully" });
+    } catch (error) {
+        console.error("Error approving application:", error);
+        return res.status(500).json({ error: "An error occurred while approving application." });
+    }
+};
+
 module.exports = {
     retrieveApplication,
-    createNewApplication
+    createNewApplication,
+    approveApplications,
 }
