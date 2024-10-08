dbcf9d5b793f4a17d7de437bcc7bec8a308b6e0f
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-08 20:05:52 +0800

    refactor: shifted helper functions and implemented data vaildation

--- backend/routes/applicationHandling.js
+++ backend/routes/applicationHandling.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { ensureLoggedIn } = require("../middlewares/authMiddleware");
+const { check, validationResult } = require("express-validator");
+const { ensureLoggedIn, ensureManager, ensureManagerAndAbove, ensureHR } = require("../middlewares/authMiddleware");
 const applicationController = require("../controllers/applicationController");
 
-router.get("/retrieveApplication", ensureLoggedIn, (req, res) => applicationController.retrieveApplication(req, res));
+// Retrieve Applications Validation Rules
+const applicationStatusValidationRules = () => {
+    return [
+        check("status").isString().isIn(['Pending', 'Approved', 'Rejected', 'Withdrawn'])
+    ];
+};
+// Validation Middleware
+const vaildateParameters = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        return next();
+    }
+    return res.status(422).json({
+        message: "Invaild Input Received",
+        errors: errors.array()
+    });
+};
+
+router.get("/retrieveApplication", applicationStatusValidationRules(), vaildateParameters, ensureLoggedIn, (req, res) => applicationController.retrieveApplication(req, res));
 router.post("/createNewApplication", ensureLoggedIn, (req, res) => applicationController.createNewApplication(req, res))
 
 module.exports = router;
