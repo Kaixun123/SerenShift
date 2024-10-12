b16a5ab1c33b82cd90f1858dafbbf93c4183f758
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-12 13:46:34 +0800

    fix: removed invaild vaildation rules for pending applications api

--- backend/routes/applicationHandling.js
+++ backend/routes/applicationHandling.js
@@ -7,50 +7,50 @@ const applicationController = require("../controllers/applicationController");
 // Retrieve Application Validation Rules
 const retrieveApplicationValidationRules = () => {
     return [
-        check("id").isInt({ allow_leading_zeroes: false, gt: 0 }),
-        check("status").isString().isIn(['Pending', 'Approved', 'Rejected', 'Withdrawn'])
+        check("id").isInt({ allow_leading_zeroes: false, gt: 0 }).withMessage("Invalid ID"),
+        check("status").isString().isIn(['Pending', 'Approved', 'Rejected', 'Withdrawn']).withMessage("Invalid Status"),
     ];
 };
 
 // Create New Application Validation Rules
 const createNewApplicationValidationRules = () => {
     return [
-        check("application_type").isString().isIn(['Regular', 'Ad Hoc']),
-        check("startDate").isISO8601().toDate(),
-        check("endDate").isISO8601().toDate(),
-        check("requestor_remarks").isString().isLength({ max: 255 }),
+        check("application_type").isString().isIn(['Regular', 'Ad Hoc']).withMessage("Invalid Application Type"),
+        check("startDate").isISO8601().toDate().withMessage("Invalid Start Date"),
+        check("endDate").isISO8601().toDate().withMessage("Invalid End Date"),
+        check("requestor_remarks").isString().isLength({ max: 255 }).withMessage("Requestor Remarks Is Too Long"),
     ];
 };
 
 // Approve Pending Application Validation Rules
 const approvePendingApplicationValidationRules = () => {
     return [
-        check("application_id").isInt({ allow_leading_zeroes: false, gt: 0 }),
-        check("approvedDates").isArray(),
-        check("approverRemarks").isString(),
+        check("application_id").isInt({ allow_leading_zeroes: false, gt: 0 }).withMessage("Invalid Application ID"),
+        check("approvedDates").isArray().withMessage("Approved Dates Must Be An Array of Dates"),
+        check("approverRemarks").isString().isLength({ max: 255 }).withMessage("Approver Remarks Is Too Long"),
     ];
 };
 
 // Reject Pending Application Validation Rules
 const rejectPendingApplicationValidationRules = () => {
     return [
-        check("application_id").isInt({ allow_leading_zeroes: false, gt: 0 }),
-        check("approverRemarks").isString(),
+        check("application_id").isInt({ allow_leading_zeroes: false, gt: 0 }).withMessage("Invalid Application ID"),
+        check("approverRemarks").isString().isLength({ max: 255 }).withMessage("Approver Remarks Is Too Long"),
     ];
 };
 
 // Withdraw Pending Application Validation Rules
 const withdrawPendingApplicationValidationRules = () => {
     return [
-        check("application_id").isInt({ allow_leading_zeroes: false, gt: 0 }),
+        check("application_id").isInt({ allow_leading_zeroes: false, gt: 0 }).withMessage("Invalid Application ID"),
     ];
 };
 
 // Withdraw Approved Application Validation Rules
 const withdrawApprovedApplicationValidationRules = () => {
     return [
-        check("application_id").isInt({ allow_leading_zeroes: false, gt: 0 }),
-        check("rejectedDates").isArray(),
+        check("application_id").isInt({ allow_leading_zeroes: false, gt: 0 }).withMessage("Invalid Application ID"),
+        check("rejectedDates").isArray().withMessage("Rejected Dates Must Be An Array of Dates"),
     ];
 };
 
@@ -68,7 +68,7 @@ const vaildateParameters = (req, res, next) => {
 };
 
 router.get("/retrieveApplication", retrieveApplicationValidationRules(), vaildateParameters, ensureLoggedIn, (req, res) => applicationController.retrieveApplications(req, res));
-router.get("/retrievePendingApplication", rejectPendingApplicationValidationRules(), vaildateParameters, ensureManagerAndAbove, (req, res) => applicationController.retrievePendingApplications(req, res));
+router.get("/retrievePendingApplication", ensureManagerAndAbove, (req, res) => applicationController.retrievePendingApplications(req, res));
 router.post("/createNewApplication", createNewApplicationValidationRules(), vaildateParameters, ensureLoggedIn, (req, res) => applicationController.createNewApplication(req, res))
 router.put("/approveApplication", approvePendingApplicationValidationRules(), vaildateParameters, ensureManagerAndAbove, (req, res) => applicationController.approvePendingApplication(req, res));
 router.put("/rejectApplication", rejectPendingApplicationValidationRules(), vaildateParameters, ensureManagerAndAbove, (req, res) => applicationController.rejectPendingApplication(req, res));
