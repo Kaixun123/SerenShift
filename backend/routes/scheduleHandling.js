3ca5c3bc9e5b765aaa6e2280c656427a02d0c76d
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-12 13:54:45 +0800

    refactor: express-vaildation for all other routes

--- backend/routes/scheduleHandling.js
+++ backend/routes/scheduleHandling.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const { ensureLoggedIn, ensureManager, ensureHR } = require("../middlewares/authMiddleware");
 const scheduleController = require("../controllers/scheduleController");
-// Validation Rules
-const employeeIdValidationRule = () => {
-    return [
-        check("id").isInt({ allow_leading_zeroes: false, gt: 0 })
-    ];
-};
+
 // Validation Middleware
 const vaildateParameters = (req, res, next) => {
     const errors = validationResult(req);
