05fb62f5c5138747c864a54e3cafc20568ebdbb4
jaye3 <jana.choa@gmail.com>
2024-10-26 00:49:14 +0800

    chore: deleted logging for debugging user details SPMPROJ-19

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -50,7 +50,6 @@ export default function SideBar() {
         });
   
         const data = await response.json();
-        console.log("DEBUG:", data);
   
         if (data && data.role) {
           setUserRole(data.role);
