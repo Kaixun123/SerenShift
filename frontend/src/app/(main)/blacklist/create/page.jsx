fd54073ff9c55fa1ef6d7d81ebeb36de22983f6d
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-25 19:55:40 +0800

    save commit

--- frontend/src/app/(main)/blacklist/create/page.jsx
+++ frontend/src/app/(main)/blacklist/create/page.jsx
@@ -1,85 +1,51 @@
 'use client'
 
 import { useState, useEffect } from 'react';
-import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer, useToast } from '@chakra-ui/react';
+import { useToast, FormControl, FormLabel, FormErrorMessage, FormHelperText, Box, Flex } from '@chakra-ui/react';
 import TopHeader from "@/components/TopHeader";
 
 export default function ManageBlacklistPage() {
     const toast = useToast();
-    const [blacklists, setBlacklists] = useState([]);
-    const retrieveBlacklists = async () => {
-        let response = await fetch('/api/blacklist/getBlacklistedDates', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
+    const [calendarValue, setCalendarValue] = useState([]);
+    const [formattedDate, setFormattedDate] = useState({
+        startDate: "",
+        endDate: "",
+    });
+    const [timeSlot, setTimeSlot] = useState("");
+    const [remarks, setRemarks] = useState("");
+    const [recurrenceRule, setRecurrenceRule] = useState("");
+    const [recurrenceEndDate, setRecurrenceEndDate] = useState("");
+    const [recurrenceError, setRecurrenceError] = useState("");
+    const handleCalendarChange = (selectedDates) => {
+        setCalendarValue(selectedDates);
+        const [newStartDate, newEndDate] = selectedDates;
+
+        setFormattedDate({
+            startDate: newStartDate ? new Date(newStartDate).toDateString() : "",
+            endDate: newEndDate ? new Date(newEndDate).toDateString() : "",
         });
-        if (response.ok) {
-            let data = await response.json();
-            setBlacklists(data);
-        } else if (response.status === 403 || response.status === 401) {
-            toast({
-                title: "Unauthorized",
-                description: "You are not authorized to view this page",
-                status: "error",
-                duration: 9000,
-                isClosable: true,
-                position: "top-right"
-            });
-        } else {
-            toast({
-                title: "Error",
-                description: "Failed to retrieve blacklist dates",
-                status: "error",
-                duration: 9000,
-                isClosable: true,
-                position: "top-right"
-            });
-        }
+    };
+    const handleTypeSelect = (e) => {
+        setType(e.target.value);
+    };
+
+    const handleTimeSlot = (e) => {
+        setTimeSlot(e.target.value);
+    };
+    const handleClear = () => {
+        setCalendarValue([]);
+        setFormattedDate({
+            startDate: "",
+            endDate: "",
+        });
+    };
+    const handleCreateBlacklist = async () => {
     }
     useEffect(() => {
-        retrieveBlacklists();
     }, []);
     return (
         <main>
-            <TopHeader mainText={`Manage Blacklist`} subText={`Control when your subordinates can submit applications`} />
-            <TableContainer>
-                <Table variant='simple'>
-                    <TableCaption>Current Blacklists</TableCaption>
-                    <Thead>
-                        <Tr>
-                            <Th>Start Date</Th>
-                            <Th>End Date</Th>
-                            <Th isNumeric></Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        <Tr>
-                            <Td>inches</Td>
-                            <Td>millimetres (mm)</Td>
-                            <Td isNumeric>25.4</Td>
-                        </Tr>
-                        <Tr>
-                            <Td>feet</Td>
-                            <Td>centimetres (cm)</Td>
-                            <Td isNumeric>30.48</Td>
-                        </Tr>
-                        <Tr>
-                            <Td>yards</Td>
-                            <Td>metres (m)</Td>
-                            <Td isNumeric>0.91444</Td>
-                        </Tr>
-                    </Tbody>
-                    <Tfoot>
-                        <Tr>
-                            <Th>To convert</Th>
-                            <Th>into</Th>
-                            <Th isNumeric>multiply by</Th>
-                        </Tr>
-                    </Tfoot>
-                </Table>
-            </TableContainer>
+
         </main>
     )
 }
