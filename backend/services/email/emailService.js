2e4153a8863da0654d4e4efa21bf1bf71bc72092
Lyney <30753183+hanlim83@users.noreply.github.com>
2024-10-29 09:33:01 +0800

    feat: initial email sending for SPMPROJ-10 and SPMPROJ-16

--- backend/services/email/emailService.js
+++ backend/services/email/emailService.js
@@ -1,47 +1,52 @@
-var nodemailer = require("nodemailer");
-const hbs = require("nodemailer-express-handlebars");
-var fs = require("fs");
-const path = require("path");
-var aws = require("@aws-sdk/client-ses");
-var { defaultProvider } = require("@aws-sdk/credential-provider-node");
+const nodemailer = require("nodemailer");
+const aws = require("@aws-sdk/client-ses");
 
 const ses = new aws.SES({
   apiVersion: "2010-12-01",
   region: "ap-southeast-1",
-  defaultProvider,
 });
 
 var transporter = nodemailer.createTransport({
   SES: { ses, aws },
 });
 
-const handlebarOptions = {
-  viewEngine: {
-    partialsDir: path.resolve("./service/email/template/"),
-    defaultLayout: false,
-  },
-  viewPath: path.resolve("./service/email/template/"),
-};
-
-transporter.use("compile", hbs(handlebarOptions));
-
-function email_broadcast(receiptent, subject, file) {
-  var mailOptions = {
-    from: "workarrangemnent_noreply@SerenShift.com",
-    bcc: receiptent,
+const send_email = async (mainRecipient, subject, message, cc, bcc) => {
+  if (mainRecipient === null || mainRecipient === "") {
+    console.error("Recipient is empty");
+    throw new Error("Recipient is empty");
+  }
+  if (subject === null || subject === "") {
+    console.error("Subject is empty");
+    throw new Error("Subject is empty");
+  }
+  if (message === null || message === "") {
+    console.error("Message is empty");
+    throw new Error("Message is empty");
+  }
+  let mailOptions = {
+    from: process.env.AWS_SES_SENDER_EMAIL,
+    to: mainRecipient,
     subject: subject,
-    html: file,
   };
-
+  if (cc)
+    mailOptions.cc = cc;
+  if (bcc)
+    mailOptions.bcc = bcc;
+  if (process.env.AWS_DUMMY_RECEIVER_EMAIL) {
+    mailOptions.text = "\n TO:" + mainRecipient + "\n CC:" + cc + "\n BCC:" + bcc + "\n" + message;
+    mailOptions.to = process.env.AWS_DUMMY_RECEIVER_EMAIL;
+  } else {
+    mailOptions.text = message;
+  }
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.log(error);
+      console.error(error);
     } else {
       console.log("Email sent: " + info.response);
     }
   });
-}
+};
 
 module.exports = {
-  email_broadcast: email_broadcast,
+  send_email
 };
